ad677ae4344e1a83955ad9c2bb865e1ff28d0bec
Ryan <betrix303@gmail.com>
2024-10-30 22:33:47 -0400

    try to fix module resolution for @ffmpeg/util

--- apps/web/app/components/trim-video-dialog.tsx
+++ apps/web/app/components/trim-video-dialog.tsx
@@ -7,9 +7,11 @@ import { Button } from "./ui/button";
 import { formatSecondsToTimestamp } from "~/lib/utils";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { fetchFile, toBlobURL } from "@ffmpeg/util";
-import { Loader, Loader2 } from "lucide-react";
+import ffmpeg from "@ffmpeg/util";
+import { Loader2 } from "lucide-react";
 import { PauseIcon, PlayIcon } from "@vidstack/react/icons";
+
+const { fetchFile, toBlobURL } = ffmpeg;
 
 export function TrimVideoDialogContainer() {
   const [trimVideoData] = useAtom(trimVideoDataAtom);
