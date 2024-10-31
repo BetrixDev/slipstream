ca8d5f724aab5b6a0f65819486e9574562a816b3
Ryan <betrix303@gmail.com>
2024-10-30 23:04:59 -0400

    update top nav styling

--- apps/web/app/components/TopNav.tsx
+++ apps/web/app/components/TopNav.tsx
@@ -12,11 +12,11 @@ import {
 export default function TopNav() {
   return (
     <div className="sticky top-2 flex justify-center z-[9999]">
-      <header className=" px-4 max-w-[48rem] w-[80%] bg-background/70 border rounded-full flex items-center justify-between h-14 reltive backdrop-blur-md shadow-sm">
+      <header className=" px-3 max-w-[48rem] w-[80%] bg-background/70 border rounded-full flex items-center justify-between h-14 reltive backdrop-blur-md shadow-sm">
         <Link className="flex items-center" to="/" prefetch="render">
           <Button variant="ghost" className="flex-shrink-0 flex items-center rounded-full">
             <Video className="h-8 w-8 text-blue-500" />
-            <span className="ml-2 text-lg md:text-2xl font-bold">Flowble</span>
+            <span className="ml-2 text-lg hidden md:inline md:text-2xl font-bold">Flowble</span>
           </Button>
         </Link>
         <div className="grow flex justify-center items-center">
@@ -47,10 +47,11 @@ export default function TopNav() {
           <div className="block md:hidden">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="outline">Navigate</Button>
+                <Button variant="outline" className="rounded-full">
+                  Navigate
+                </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuItem></DropdownMenuItem>
                 <DropdownMenuItem>
                   <Link
                     className="text-sm font-medium hover:underline underline-offset-4"
@@ -79,16 +80,43 @@ export default function TopNav() {
             </DropdownMenu>
           </div>
         </div>
-        <div className="flex gap-2 w-40 justify-end z-10">
-          <SignedIn>
-            <span className="md:flex hidden items-center">
-              <UserButton />
-            </span>
-            <span className="flex md:hidden items-center">
-              <UserButton />
-            </span>
-          </SignedIn>
-          <SignedOut>
+        <SignedOut>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="rounded-full">
+                Account
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link
+                  className="text-sm font-medium hover:underline underline-offset-4"
+                  to="/sign-in"
+                >
+                  Sign in
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  className="text-sm font-medium hover:underline underline-offset-4"
+                  to="/sign-up"
+                >
+                  Sign Up
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SignedOut>
+        <SignedIn>
+          <span className="md:flex hidden items-center">
+            <UserButton />
+          </span>
+          <span className="flex md:hidden items-center">
+            <UserButton />
+          </span>
+        </SignedIn>
+        <SignedOut>
+          <div className="hidden md:flex gap-2 w-40 justify-end z-10">
             <Link to="/sign-in">
               <Button className="rounded-full" variant="ghost">
                 Sign in
@@ -99,8 +127,8 @@ export default function TopNav() {
                 Sign Up
               </Button>
             </Link>
-          </SignedOut>
-        </div>
+          </div>
+        </SignedOut>
       </header>
     </div>
   );
