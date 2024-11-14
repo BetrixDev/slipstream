bbee8bc6f7c8e1c562bd6192bbb39a1e7f5ac4cb
Ryan <betrix303@gmail.com>
2024-11-13 20:11:05 -0500

    Update env.ts

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,21 +1,21 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  S3_REGION: z.string(),
-  S3_ENDPOINT: z.string(),
-  S3_READ_ONLY_ACCESS_KEY: z.string(),
-  S3_READ_ONLY_SECRET_KEY: z.string(),
-  S3_ROOT_ACCESS_KEY: z.string(),
-  S3_ROOT_SECRET_KEY: z.string(),
-  VIDEOS_BUCKET_NAME: z.string(),
-  THUMBS_BUCKET_NAME: z.string(),
-  THUMBNAIL_BASE_URL: z.string(),
-  REDIS_REST_URL: z.string(),
-  REDIS_REST_TOKEN: z.string(),
-  STRIPE_SECRET_KEY: z.string(),
-  STRIPE_SIGNING_SECRET: z.string(),
-  PREMIUM_PRODUCT_ID: z.string(),
-  PRO_PRODUCT_ID: z.string(),
+  S3_REGION: z.string().default(""),
+  S3_ENDPOINT: z.string().default(""),
+  S3_READ_ONLY_ACCESS_KEY: z.string().default(""),
+  S3_READ_ONLY_SECRET_KEY: z.string().default(""),
+  S3_ROOT_ACCESS_KEY: z.string().default(""),
+  S3_ROOT_SECRET_KEY: z.string().default(""),
+  VIDEOS_BUCKET_NAME: z.string().default(""),
+  THUMBS_BUCKET_NAME: z.string().default(""),
+  THUMBNAIL_BASE_URL: z.string().default(""),
+  REDIS_REST_URL: z.string().default(""),
+  REDIS_REST_TOKEN: z.string().default(""),
+  STRIPE_SECRET_KEY: z.string().default(""),
+  STRIPE_SIGNING_SECRET: z.string().default(""),
+  PREMIUM_PRODUCT_ID: z.string().default(""),
+  PRO_PRODUCT_ID: z.string().default(""),
 });
 
 export const env = envSchema.parse(process.env);
