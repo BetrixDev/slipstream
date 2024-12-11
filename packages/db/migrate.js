bb2f1c9e4bf57e9a79d3658d6eb2a574c2334325
Ryan <betrix303@gmail.com>
2024-12-11 17:08:29 -0500

    trying to use the actual neon driver again

--- packages/db/migrate.js
+++ packages/db/migrate.js
@@ -1,10 +1,10 @@
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import path from "path";
 
-const queryClient = postgres(process.env.DATABASE_URL);
-export const db = drizzle(queryClient);
+const neonClient = neon(process.env.DATABASE_URL);
+export const db = drizzle(neonClient);
 
 await migrate(db, {
   migrationsFolder: path.join(process.cwd(), "migrations"),
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,9 @@
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema.js";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const queryClient = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(queryClient, { schema });
+const neonClient = neon(process.env.DATABASE_URL!);
+export const db = drizzle(neonClient, { schema });
 
 export * from "./schema.js";
 export * from "drizzle-orm";
